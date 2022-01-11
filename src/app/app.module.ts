84256c5f02948bfd07ca5f9430d48f206e76a396
Nadia <nadejda_egorova92@mail.ru>
2022-01-10 16:14:33 -0800

    app component was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CompaniesComponent } from './companies/companies.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompaniesComponent,
   ],
   imports: [
     BrowserModule
